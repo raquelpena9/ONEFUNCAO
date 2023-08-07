3e178c20e48a1e1124439a36223f46011a70281c
Raquel Pena <139807495+raquelpena9@users.noreply.github.com>
2023-08-07 16:20:42 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function Soma (a,b) {
+    if (a + b === 17) {
+    console.log(a + b +" e igual 17.");
+    } else {
+     console.log (a + b +" nao é igual 17.");
+    }
+}
+    
+console.log(Soma(5,9))
